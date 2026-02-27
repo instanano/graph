6ebd880d645244f156484a0a97bc09e95d1e365d
InstaNANO <52311496+instanano@users.noreply.github.com>
2026-02-27 19:28:29 +0530

    Update shapes.js

--- features/shapes.js
+++ features/shapes.js
@@ -123,10 +123,14 @@
             } makeShapeInteractive(g); setTimeout(() => G.features.activateShape(g), 0); G.state.tempShape=null;
             } d3.selectAll('input[name="shape"]').property('checked', false);G.state.shapeMode = "none"; });
     }
-    G.ui.refs.addTextBtn.on("click",function(){ G.ui.disableAreaCal();
+    function addUserText(rotation){
+        G.ui.disableAreaCal();
         const svg=d3.select("#chart svg");if(svg.empty())return;
-        const {fo,div} = G.utils.editableText(svg,{x:G.config.DIM.W/2-G.config.DIM.MT,y:G.config.DIM.H/2-G.config.DIM.MR,text:"Text",rotation:0});
-        fo.classed("user-text",1).call(G.utils.applyDrag); G.utils.clearActive(); G.features.activateText(div, fo);});
+        const {fo,div} = G.utils.editableText(svg,{x:G.config.DIM.W/2-G.config.DIM.MT,y:G.config.DIM.H/2-G.config.DIM.MR,text:"Text",rotation});
+        fo.classed("user-text",1).call(G.utils.applyDrag); G.utils.clearActive(); G.features.activateText(div, fo);
+    }
+    G.ui.refs.addTextBtn.on("click",function(){ addUserText(0); });
+    G.ui.refs.addTextVBtn.on("click",function(){ addUserText(-90); });
     d3.selectAll('input[name="shape"]').on("change", function(){ G.ui.disableAreaCal(); G.state.shapeMode = this.value; G.utils.clearActive();});
     G.features.makeShapeInteractive = makeShapeInteractive; 
 })(window.GraphPlotter);
